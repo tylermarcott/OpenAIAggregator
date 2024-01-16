fb2be2c322407b91603d94d3021c4e4c91cdbe7b
Tyler Marcott <tylerjmarcott@gmail.com>
2024-01-16 00:24:14 -0700

    init commit

---
+++ client/src/pages/HomePage.vue
@@ -0,0 +1,25 @@
+<template>
+  <section>
+    <div class="row">
+      <div class="col-12">
+        <h3>
+          Open AI Aggregator Project
+        </h3>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
